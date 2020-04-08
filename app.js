08004fab0ff9ed99265fedb4b134ee421b8c3ccf
Tooba Ali <toobaalined@gmail.com>
2020-04-08 19:39:46 +0500

    Add separate lists for chores and work

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine" , "ejs");
 app.use(express.static("public"));
 
-let items = ["Wash Clothes"];
+let items = ["Do Homework"];
+let workItems = [];
+let choreItems = [];
 
 app.get("/", function(req,res){
     
@@ -18,8 +20,30 @@ app.get("/", function(req,res){
 
 app.post("/", function(req,res){
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if(req.body.submitBtn === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+
+    else if(req.body.submitBtn === "Chores"){
+        choreItems.push(item);
+        res.redirect("/chores");
+    }
+
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+   
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle:"Work List", listItem: workItems });
+});
+
+app.get("/chores", function(req,res){
+    res.render("list", {listTitle:"Chores List", listItem: choreItems });
 });
 
 app.listen(3000, function(){
