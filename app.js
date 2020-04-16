d09c29ab71380863df3eb71373917f21ead6dd52
Tooba Ali <toobaalined@gmail.com>
2020-04-16 12:11:57 +0500

    Add and delete from tasks page with mongodb

--- app.js
+++ app.js
@@ -1,48 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname+"/date.js");
-
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine" , "ejs");
 app.use(express.static("public"));
 
-const items = [];
-const workItems = [];
-const choreItems = [];
+mongoose.connect("mongodb://localhost:27017/todoDB", {useNewUrlParser:true, useUnifiedTopology:true});
+
+// make item schema
+const itemSchema = {
+    name: String
+}
+
+// make item model
+const Item = mongoose.model("Item", itemSchema);
 
 let day = date.getDate();
 app.get("/", function(req,res){
-    res.render("list", {listTitle: "Tasks", listItem: items , dateTitle: day});
+    Item.find({},function(err, foundTaskList){
+       if(err){
+        console.log(err);
+       }
+        else{
+           
+            res.render("list", {listTitle: "Tasks", listItem: foundTaskList , dateTitle: day});
+        }
+        
+    });
 });
 
 app.post("/", function(req,res){
-    const item = req.body.newItem;
 
-    if(req.body.submitBtn === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
+    const item = new Item({
+        name: req.body.newItem
+    });
+
+    if(req.body.submitBtn === "Tasks") {
+        item.save();
+        res.redirect("/");
     }
 
     else if(req.body.submitBtn === "Chores"){
         choreItems.push(item);
         res.redirect("/chores");
     }
-
-    else{
-        items.push(item);
-        res.redirect("/");
-    }
    
 });
 
-app.get("/work", function(req,res){
-    res.render("list", {listTitle:"Work List", listItem: workItems, dateTitle: day });
-});
-
-app.get("/chores", function(req,res){
-    res.render("list", {listTitle:"Chores List", listItem: choreItems, dateTitle: day });
+app.post("/delete", function(req,res){
+    const checkedID = req.body.submitCheckbox;
+    Item.findByIdAndRemove(checkedID, function(err){});
+    res.redirect("/")
+    
 });
 
 app.listen(3000, function(){
